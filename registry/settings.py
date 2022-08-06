e6d2b981dd19f1338870f763e18a73a101ef66da
rezyte <alirezaalavirazavi@yahoo.com>
2022-08-06 11:08:55 +0430

    static and media settings added

--- registry/settings.py
+++ registry/settings.py
@@ -130,16 +130,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "/home/algotiki/django/static"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = "/home/algotiki/django/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MOLLIE_API_KEY = "test_8cSdQJbytTJmSPS5kWxUhtDSRQ9ChC"
-
-DEFAULT_MAX_DIGITS = 15
-DEFAULT_DECIMAL_PLACES = 3
 
 AUTH_USER_MODEL = "account.User"
 
