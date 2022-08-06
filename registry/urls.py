59d46b9e2f2262942cadf176c7681e5e61c85afc
rezyte <alirezaalavirazavi@yahoo.com>
2022-08-06 11:21:33 +0430

    fix static urls

--- registry/urls.py
+++ registry/urls.py
@@ -8,8 +8,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("order.urls")),
     path("", include("account.urls")),
-] + [
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) ,
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-]
+] 
 
+urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
