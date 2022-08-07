39df5f23d63c1c21a747145c4806dfbfd33899d3
rezyte <alirezaalavirazavi@yahoo.com>
2022-08-08 00:06:11 +0430

    fix the product slug bug

--- products/serializers.py
+++ products/serializers.py
@@ -36,7 +36,6 @@ class ProductSerializer(serializers.ModelSerializer):
     permissions = ProductPermissionSerializer(many=True, read_only=True)
     files = ProductFileSerializer(many=True, read_only=True)
     codes = PieceOfCodeSerializer(many=True, read_only=True)
-    slug = serializers.CharField(required=False)
 
     class Meta:
         model = Product
