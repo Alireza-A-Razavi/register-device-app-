294db39e0c83d0fbda4c56b4d1799bb04bb50fdb
Alireza-A-Razavi <65015631+Alireza-A-Razavi@users.noreply.github.com>
2022-08-22 13:10:16 +0430

    change products/id url typo

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("api/product/create/", views.ProductReplicaCreateAPIView.as_view()),
 
-    path("api/proudct/<wp_product_id>/", views.ProductRetrieveAPIVIew.as_view()),
+    path("api/product/<wp_product_id>/", views.ProductRetrieveAPIVIew.as_view()),
 
 ]    
 
